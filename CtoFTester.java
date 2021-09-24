00629bd68a5ae9fb6eebc6f073ad4670d113d9cb
Eugene Yoo <eugeneyoo@eugenes-mbp-2.home>
2021-09-23 22:51:36 -0400

    Coded how to convert from celsius to fahrenheit and vice versa

--- CtoFTester.java
+++ CtoFTester.java
@@ -7,10 +7,16 @@ as well because its input is a double.
 public class CtoFTester{
   public static void main(String[] args) {
     double x = celsiusToFahrenheit(32.3);
+    double y = fahrenheitToCelsius(40.1);
     System.out.printf("Your temperature in Farenheit is " + x);
+    System.out.printf("Your temperature in Celsius is " + y);
   }
 
   private static double celsiusToFahrenheit(double c){
     return c * 9.0 / 5.0 + 32;
+  }
+
+  private static double fahrenheitToCelsius(double f){
+    return (f - 32) * 5.0 / 9.0;
   }
 }
